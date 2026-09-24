725be2f53680613764d5f5e15d0588b4d67301d8
grkmuysl <gorkmuysal@gmail.com>
2026-09-24 20:17:01 +0300

    Using Retry Table Topic
    I added RetryableTopic to Consumer. I made this for the specifie attemps count and delay

--- src/main/java/com/gorkemuysal/notificationservice/notification/BirthdayEventConsumer.java
+++ src/main/java/com/gorkemuysal/notificationservice/notification/BirthdayEventConsumer.java
@@ -2,7 +2,11 @@ package com.gorkemuysal.notificationservice.notification;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.BackOff;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -12,11 +16,23 @@ public class BirthdayEventConsumer {
 
     private final EmailNotificationService emailNotificationService;
 
+    @RetryableTopic(
+            attempts = "4",
+            backOff = @BackOff(delay = 2000, multiplier = 2.0),
+            dltTopicSuffix = "-dlt",
+            include = { Exception.class }
+    )
     @KafkaListener(topics = "birthday.upcoming", groupId = "notification-service")
     public void handle(UpcomingBirthdayEvent event) {
         log.info("Event received: Person {}'s  birthday is in {} days({})",
                 event.fullName(), event.daysUntil(), event.birthDate());
 
         emailNotificationService.sendBirthdayReminder(event);
+    }
+
+
+    @DltHandler
+    public void handleDlt(UpcomingBirthdayEvent event) {
+        log.error("Entering to DLT, All retries are failed: Person- {}: {}", event, event.fullName());
     }
 }
