f1fec98b89c48fffab23a9c19d4cb559fb54be8d
grkmuysl <gorkmuysal@gmail.com>
2026-09-24 19:12:02 +0300

    Creating new Service and Use Mail Hog
    1- I implemented Email Notification Service to send an email for the upcoming birthdays
    2- I used Mail Hog as a Test Stmp host. Thanks to Mail Hog I can easily test sending email process

--- src/main/java/com/gorkemuysal/notificationservice/notification/BirthdayEventConsumer.java
+++ src/main/java/com/gorkemuysal/notificationservice/notification/BirthdayEventConsumer.java
@@ -1,15 +1,22 @@
 package com.gorkemuysal.notificationservice.notification;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class BirthdayEventConsumer {
+
+    private final EmailNotificationService emailNotificationService;
+
     @KafkaListener(topics = "birthday.upcoming", groupId = "notification-service")
     public void handle(UpcomingBirthdayEvent event) {
         log.info("Event received: Person {}'s  birthday is in {} days({})",
                 event.fullName(), event.daysUntil(), event.birthDate());
+
+        emailNotificationService.sendBirthdayReminder(event);
     }
 }
